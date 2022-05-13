0f2113b5dbc937c9179ccb8426443455dbd10f6d
Max Prokopiev <maxprokopiev@github.com>
2022-05-13 19:30:25 +0200

    Add vscode settings, more aliases, install vim-plug

--- install.sh
+++ install.sh
@@ -13,4 +13,9 @@ sudo apt-get -y install neovim
 ln -fs $PWD/zsh/.zsh $HOME/.zsh
 ln -fs $PWD/zsh/pure/pure.zsh $HOME/.zsh/prompt_pure_setup
 ln -fs $PWD/zsh/.zshrc $HOME/.zshrc
+
+mkdir -p $HOME/.config/nvim
 ln -fs $PWD/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+
+mkdir -p $HOME/.config/Code/User
+ln -fs $PWD/vscode/settings.json $HOME/.config/Code/User/settings.json
