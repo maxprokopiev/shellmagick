009822611c7459058a83101e56cbf7e68d81a172
Max Prokopiev <maxprokopiev@github.com>
2022-05-16 17:14:25 +0200

    Better way to install packages

--- install.sh
+++ install.sh
@@ -6,7 +6,17 @@ set -x
 
 git submodule update --init
 
-sudo apt-get -y install neovim
+PACKAGES_NEEDED="\
+    neovim \
+    bat"
+
+if ! dpkg -s ${PACKAGES_NEEDED} > /dev/null 2>&1; then
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        sudo apt-get update
+    fi
+    sudo echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+    sudo apt-get -y -q install ${PACKAGES_NEEDED}
+fi
 
 ln -fs $PWD/zsh/.zsh $HOME/.zsh
 ln -fs $PWD/zsh/pure/pure.zsh $HOME/.zsh/prompt_pure_setup
@@ -14,7 +24,7 @@ ln -fs $PWD/zsh/.zshrc $HOME/.zshrc
 
 mkdir -p $HOME/.config/nvim
 ln -fs $PWD/.config/nvim/init.vim $HOME/.config/nvim/init.vim
-vim +PluginInstall +qall
+nvim +'PlugInstall --sync' +qa
 
 mkdir -p $HOME/.config/Code/User
 ln -fs $PWD/vscode/settings.json $HOME/.config/Code/User/settings.json
