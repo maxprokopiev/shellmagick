c557fd1725a1591fbb00a771c14f55e1f6310cf0
Max Prokopiev <maxprokopiev@github.com>
2022-05-16 15:49:07 +0200

    Fix shell settings

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# use zsh by default
-if ! grep -q "root.*/bin/zsh" /etc/passwd
-then
-  chsh -s /bin/zsh root
-fi
-
 git submodule update --init
 
 sudo apt-get -y install neovim
@@ -19,3 +13,6 @@ ln -fs $PWD/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 
 mkdir -p $HOME/.config/Code/User
 ln -fs $PWD/vscode/settings.json $HOME/.config/Code/User/settings.json
+
+# use zsh by default
+sudo chsh -s "$(which zsh)" "$(whoami)"
