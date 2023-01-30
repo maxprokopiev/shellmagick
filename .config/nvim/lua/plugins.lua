8e52d1ae277d6f76977a926a687979dedb00b418
Max Prokopiev <maxprokopiev@github.com>
2023-01-30 15:57:48 +0100

    Fix theme version

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
-  use 'projekt0n/github-nvim-theme'
+  use { 'projekt0n/github-nvim-theme', tag = 'v0.0.7' }
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
