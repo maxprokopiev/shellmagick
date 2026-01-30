cb346cf73b18900cc0ba1e73b562d911b9c2ebb3
Max Prokopiev <maxprokopiev@github.com>
2026-01-30 15:51:28 +0100

    Enable hidden files in telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -80,3 +80,17 @@ if ts_status then
     highlight = { enable = true },
   })
 end
+
+require("telescope").setup{
+  pickers = {
+    find_files = {
+      hidden = true
+    },
+    grep_string = {
+      additional_args = {"--hidden"}
+    },
+    live_grep = {
+      additional_args = {"--hidden"}
+    },
+  },
+}
